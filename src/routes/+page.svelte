7ebe325304c3944931407f982040c58a5483912c
Ben Prize Bar-On <prizeben@gmail.com>
2024-02-24 15:02:32 +0200

    Read expense sources from API

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-  import { ExpenseSource, RecurringMoneyValue, Period } from '$lib/domain';
-	import ExpenseSourcesTable from '$lib/expenseSourcesTable.svelte';
+  import ExpenseSourcesTable from '$lib/expenseSourcesTable.svelte';
+  import type { PageData } from './$types';
 
-  let expenseSources = [
-    new ExpenseSource(1, "הוצאה 1", new RecurringMoneyValue(10, new Period("Month", 2))),
-    new ExpenseSource(2, "הוצאה 2", new RecurringMoneyValue(40, new Period("Year", 2))),
-  ];
+  export let data: PageData;
+
+  let { expenseSources } = data;
 </script>
 
 <ExpenseSourcesTable {expenseSources} />
