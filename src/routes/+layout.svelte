feae704818c58f092f39ad2ea6c379834f647e09
Ben Prize Bar-On <prizeben@gmail.com>
2024-03-24 20:48:37 +0200

    Add sign-in and sign-out with local keycloak

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,24 @@
-<script>
-	import { DarkMode } from 'flowbite-svelte';
+<script lang="ts">
 	import '../app.pcss';
+	import { Button, DarkMode } from 'flowbite-svelte';
+	import { page } from '$app/stores';
+	import type { Session, User } from '@auth/sveltekit';
+	import { signIn, signOut } from '@auth/sveltekit/client';
+
+	let session: Session | null | undefined = $page.data.session;
+	let user: User | undefined = session?.user;
 </script>
 
-<DarkMode />
-<slot />
+<div class="dark:text-gray-400">
+	<div>
+		<DarkMode />
+		{#if session}
+			<span>{user?.name ?? 'User'}</span>
+			<Button type="button" on:click={() => signOut()} color="red">Sign out</Button>
+		{:else}
+			<span>You are not signed in</span>
+			<Button type="button" on:click={() => signIn('keycloak')} color="green">Sign in</Button>
+		{/if}
+	</div>
+	<slot />
+</div>
