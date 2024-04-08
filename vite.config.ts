5a45a1abb775a0571755dec862bde162a9dc3b53
Ben Prize Bar-On <prizeben@gmail.com>
2024-04-08 17:09:31 +0300

    Reload on locale change
    Add plugin that reloads the server when JSON files in the `src`
    directory change.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
+
+function jsonHotReload(): PluginOption {
+  return {
+    name: 'json-hot-reload',
+    async handleHotUpdate({ file, server }) {
+      const srcDirectory = __dirname + '/src/';
+      if (file.startsWith(srcDirectory) && file.endsWith('.json')) {
+        await server.restart();
+      }
+    },
+  }
+}
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit(), jsonHotReload()]
 });
